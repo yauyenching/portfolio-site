1017f388b61537c2b4ba6e5a264678934d7b7914
Yau Yen Ching <yauyenching@pm.me>
2023-09-28 23:30:51 +0800

    feat: Add PWA capabilities to site
    - Add maskable icons
    
    - Change title and meta description
    
    - Add service workers with offline caching functionality

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,9 +2,17 @@ import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import Script from 'next/script'
 import { ChakraBaseProvider } from '@chakra-ui/react'
+import { useEffect } from 'react'
 import theme from 'styles/theme'
 
 function MyApp({ Component, pageProps }: AppProps) {
+  useEffect(() => {
+    if ('serviceWorker' in navigator) {
+      navigator.serviceWorker
+        .register('/service-worker.js')
+    }
+  }, []);
+
   return (
     <ChakraBaseProvider theme={theme} resetCSS={false}>
       <Component {...pageProps} />
